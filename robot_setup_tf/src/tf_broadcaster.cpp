c9ebbec4e2b6243ab634b59c8ee0a26e9302f129
cfuhrman-utcr2018 <cfuhrman@citadel.edu>
2018-01-24 15:51:41 -0500

    Working towards the laser transform.

--- robot_setup_tf/src/tf_broadcaster.cpp
+++ robot_setup_tf/src/tf_broadcaster.cpp
@@ -1,13 +1,26 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
+#include <std_msgs/String.h>
+#include <sensor_msgs/LaserScan.h>
 
+
+
+ros::NodeHandle n;
+ros::Subscriber = n.subscribe<sensor_msgs::LaserScan>("/scan", 10, &AutoExp::laserCallback, this);
+
+void laserCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
+{
+  // ROS_INFO("I heard: [%s]", msg->data.c_str());
+}
+	
+	
 int main(int argc, char** argv){
   ros::init(argc, argv, "robot_tf_publisher");
-  ros::NodeHandle n;
-
+  
   ros::Rate r(100);
 
   tf::TransformBroadcaster broadcaster;
+  
 
   while(n.ok()){
     broadcaster.sendTransform(
@@ -16,4 +29,6 @@ int main(int argc, char** argv){
         ros::Time::now(),"base_link", "base_laser"));
     r.sleep();
   }
+  
+  ros::spin();
 }
