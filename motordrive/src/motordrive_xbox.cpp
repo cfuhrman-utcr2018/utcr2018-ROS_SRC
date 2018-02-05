f08f83d1631725c1dd3fff23873bc5cddf608529
cfuhrman-utcr2018 <cfuhrman@citadel.edu>
2018-02-05 16:07:21 -0500

    Added a stop and start button. Cleaning up code for readability and stability.

--- motordrive/src/motordrive_xbox.cpp
+++ motordrive/src/motordrive_xbox.cpp
@@ -1,13 +1,34 @@
-/* Comment here */
+/* Code to control the IGVC 2018 entry from The Citadel with a wireless Xbox 360 Controller
+Requirements: joy_node
+rosserial_arduino node
+Xbox 360 controller
+Arduino
+Motor Controllers (NAME???)
+================================================================================================================
+Author: Connor Fuhrman
+Contact: 512-897-0709
+Contact: cfuhrman@citadel.edu
+Contact: connorfuhrman@outlook.com
+================================================================================================================
+List of Variables:
+- Global
+	- Start
+- Local
+	- 
+
+ROS Subscribers:
+
+ROS Publishers:
+*/
 
 #include "ros/ros.h"
 #include "std_msgs/Float64.h"
 #include <std_msgs/UInt16.h>
 #include <sensor_msgs/Joy.h>
-
-
 #include <array>
 #include <vector>
+
+int Start = 0; // the emergency stop button
 
 class SubscribeAndPublish
 {
@@ -23,20 +44,24 @@ class SubscribeAndPublish
 	void joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 	{
 
-		std_msgs::Float64 velocity_L; // the speed as a function of direction
+		typedef std_msgs::Float64 ROSfloat;
+		typedef std_msgs::UInt16 ROSint;
+
+		ROSfloat velocity_L; // the speed as a function of direction
 			// iniialized to 0 so that the robot will be still initially
-		std_msgs::Float64 velocity_R;
-		std_msgs::UInt16 duty_cycle_L; // the duty cycle as a function of speed (+ or -) and velocity
+		ROSfloat velocity_R;
+		ROSint duty_cycle_L; // the duty cycle as a function of speed (+ or -) and velocity
 			// set to 158 so that the motor controllers will keep the wheels stationary
-		std_msgs::UInt16 duty_cycle_R;
-		std_msgs::Float64 speed;
-		std_msgs::Float64 direction;
-		std_msgs::Float64 A;
-		std_msgs::Float64 B;
+		ROSint duty_cycle_R;
+		ROSfloat speed;
+		ROSfloat direction;
+		ROSfloat A;
+		ROSfloat B;
 		
 		// initializing the maximum and minimum multipliers for the velocity commands acting on the speed command
-		float Turn_Max = 0.75;
-		float Turn_Min = 1.0-Turn_Max;
+		double Turn_Max = 0.75;
+		double Turn_Min = 1.0-Turn_Max;
+		
 	
 		// initialize the speed and direction from the joystick commands
 		speed.data = joy->axes[1];
@@ -44,43 +69,55 @@ class SubscribeAndPublish
 		// initialize the start and stop buttons A and B
 		A.data = joy->buttons[0];
 		B.data = joy->buttons[1];
-/*
-		// wait for the A button to be pressed
-		go_button:
-		while ( A < 0.9 ){
-			// do nothing
+
+		
+		if ( A.data > 0.9 ){
+			Start = 1;
 		}
 		
-		// B button stops the robot
-		if ( B > 0.8 ){
-			duty_cycle = 158;
-			goto go_button;
-		}
-*/	
-		
-		// calculate the velocity based on the direction input (right stick)
-		if ( direction.data > 0.0 ){
-			velocity_L.data = speed.data * (1.0-Turn_Max*direction.data);
-			velocity_R.data = speed.data * (1.0-Turn_Min*direction.data); 
-		} else if ( direction.data < 0.0 ) {
-			velocity_L.data = speed.data * (1.0+Turn_Min*direction.data);
-			velocity_R.data = speed.data * (1.0+Turn_Max*direction.data);
+		if ( Start == 1){
+		// invariant is the Start variable. Exit the while loop by
+		// pressing B
+
+			if ( B.data > 0.9){
+			 	Start = 0;
+			 }
+
+			// calculate the velocity based on the direction input (right stick)
+			if ( direction.data > 0.0 ){
+				velocity_L.data = speed.data * (1.0-Turn_Max*direction.data);
+				velocity_R.data = speed.data * (1.0-Turn_Min*direction.data); 
+			} else if ( direction.data < 0.0 ) {
+				velocity_L.data = speed.data * (1.0+Turn_Min*direction.data);
+				velocity_R.data = speed.data * (1.0+Turn_Max*direction.data);
+			} else {
+				velocity_L.data = speed.data;
+				velocity_R.data = speed.data;
+			} 
+			
+			if ( speed.data > 0.0 ){
+				duty_cycle_L.data = 91.8*velocity_L.data+163.2;
+				duty_cycle_R.data = 91.8*velocity_R.data+163.2;
+			} else {
+				duty_cycle_L.data = 153.0*velocity_L.data+153.0;
+				duty_cycle_R.data = 153.0*velocity_R.data+153.0;
+			}
+			
+			// Publish the left and right duty cycle
+			pub_DutyCycle_L.publish(duty_cycle_L);
+			pub_DutyCycle_R.publish(duty_cycle_R);
+
 		} else {
-			velocity_L.data = speed.data;
-			velocity_R.data = speed.data;
-		} 
+		// if we are not moving, the duty should be 154
+		duty_cycle_L.data = 154;
+		duty_cycle_R.data = duty_cycle_L.data;
 		
-		if ( speed.data > 0.0 ){
-			duty_cycle_L.data = 91.8*velocity_L.data+163.2;
-			duty_cycle_R.data = 91.8*velocity_R.data+163.2;
-		} else {
-			duty_cycle_L.data = 153.0*velocity_L.data+153.0;
-			duty_cycle_R.data = 153.0*velocity_R.data+153.0;
-		}
-		
+		// Publish the left and right duty cycle
 		pub_DutyCycle_L.publish(duty_cycle_L);
 		pub_DutyCycle_R.publish(duty_cycle_R);
 		
+		}
+
 	}
 
 	private:
@@ -101,5 +138,3 @@ int main(int argc, char **argv)
 	return 0;
 
 }
-
-
