1318dac68578b1b9076449d36873cd3b427cb9e4
Connor <cfuhrman@citadel.edu>
2018-04-03 11:49:59 -0400

    MATLAB reading and processing live data

---
+++ matlab/gazebosim_image_processing.m
@@ -0,0 +1,20 @@
+% This program connects to a Gazebo simulation within ROS and pulls
+% image data from the camera continuously. It impliments an image 
+% processing algorithm to display all white.
+
+clear, clc, close all
+
+%rosinit
+
+im_sub = rossubscriber('/camera/image_raw');
+while 1
+    im_ros = receive(im_sub,10);
+    im = readImage(im_ros);
+    gray = rgb2gray(im);
+    % imshow(im)
+    TH = (gray > 150);
+    BW = medfilt2(TH);
+    F = bwareaopen(BW,30);
+    imshow(F);
+    drawnow;
+end
