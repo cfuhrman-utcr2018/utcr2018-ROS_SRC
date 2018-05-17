96f182521ce278cd5afda1e8d26a8b52c7d2b56e
Connor <cfuhrman@citadel.edu>
2018-05-17 14:49:27 -0400

    sync

--- matlab/Scripts/RealSense Lane Detect/realsense_line_detect.m
+++ matlab/Scripts/RealSense Lane Detect/realsense_line_detect.m
@@ -14,49 +14,14 @@ rosinit
 
 addpath(genpath('Functions')) % to use functions in different folder
 
-% File for testing with sample ROS PointCloud2 variable
-load ~/catkins_ws/src/matlab/'Saved Variables'/ROS_PointCloud2.mat
+% % File for testing with sample ROS PointCloud2 variable
+% load ~/catkins_ws/src/matlab/'Saved Variables'/ROS_PointCloud2.mat
 
 % Initiailze variables:
 threshold_value = 0.7;
 
-% % create subscriber to depth cloud:
-% PointCloud_sub = rossubscriber('/camera/depth_registered/points');
-% 
-% % create published of processed data
-% lines_msg = rosmessage('sensor_msgs/PointCloud');
-% lines_msg.Header.FrameId = 'line_camera';
-% lines_pub = rospublisher('/lines','sensor_msgs/PointCloud');
-% 
-% % Loop to here!!
-% 
-% 
-% % Get data from ROS
-% ptcloud_ros = receive(PointCloud_sub,10); % recieve the ROS cloud
-% ptcloud_ros.PreserveStructureOnRead = true;
-
-im_ros = readRGB(ptcloud_ros); % create an rgb image from PointCloud
-depth_ros = readXYZ(ptcloud_ros); % create depth mapping from PointCloud
-
-% Detect white lines and return an image showing only white lines:
-lines_image = process_image(im_ros, threshold_value);
-
-% % create an RBG point cloud containing detected lines
-% ptcloud = process_ptcloud(im_ros, depth_image, lines_image); % For 
-%     % creating a MATLAB ptcloud to visualize data within MATLAB
-%     figure
-% pcshow(ptcloud);
-
-% Create an XYZ matrix containing only lines
-xyz_lines = depth_ros.*lines_image;
-
-% % Convert xyz from a MxNx3 matrix to a (M*N)x3 matrix as if the
-% % PreserveStructureOnRead was set to FALSE
-% xyz_Dx3 = mxnx3_to_dx3(xyz);
-% 
-% % Convert XYZ matrix into /sensor_msgs/PointCloud
-% lines_msg = xyz_to_PointCloud(xyz_Dx3, lines_msg);
-% lines_msg.Header.Stamp = rostime('now');
+% create subscriber to depth cloud:
+PointCloud_sub = rossubscriber('/camera/depth_registered/points');
 
 % create messages to pass the depth and rbg images
 depth = rosmessage('sensor_msgs/Image');
@@ -68,8 +33,38 @@ color_pub = rospublisher('/processed_color', 'sensor_msgs/Image');
 color.Encoding = '64fc3';
 depth.Encoding = '32fc3';
 
-writeImage(depth, xyz_lines);
-writeImage(color, lines_image);
+color.Header.FrameId = 'lines_camera';
+depth.Header.FrameId = 'lines_camera';
 
-send(depth_pub, depth);
-send(color_pub, color);
+while 1
+
+    % Get data from ROS
+    ptcloud_ros = receive(PointCloud_sub,10); % recieve the ROS cloud
+    ptcloud_ros.PreserveStructureOnRead = true;
+
+    im_ros = readRGB(ptcloud_ros); % create an rgb image from PointCloud
+    depth_ros = readXYZ(ptcloud_ros); % create depth mapping from PointCloud
+
+    % Detect white lines and return an image showing only white lines:
+    lines_image = process_image(im_ros, threshold_value);
+
+    % % create an RBG point cloud containing detected lines
+    % ptcloud = process_ptcloud(im_ros, depth_image, lines_image); % For 
+    %     % creating a MATLAB ptcloud to visualize data within MATLAB
+    %     figure
+    % pcshow(ptcloud);
+
+    % Create an XYZ matrix containing only lines
+    xyz_lines = depth_ros.*lines_image;
+    
+    time = rostime(now);
+    color.Header.Stamp = time;
+    depth.Header.Stamp = time;
+
+    writeImage(depth, xyz_lines);
+    writeImage(color, lines_image);
+
+    send(depth_pub, depth);
+    send(color_pub, color);
+
+end
