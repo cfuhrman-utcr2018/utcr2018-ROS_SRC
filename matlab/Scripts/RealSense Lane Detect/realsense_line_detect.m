5115331e2f3564cc012950900ad12eb00e987e79
Connor <cfuhrman@citadel.edu>
2018-05-23 09:09:57 -0400

    depth_image_proc working (will not subscribe until subscriber created for output)

--- matlab/Scripts/RealSense Lane Detect/realsense_line_detect.m
+++ matlab/Scripts/RealSense Lane Detect/realsense_line_detect.m
@@ -15,14 +15,14 @@ rosinit
 addpath(genpath('Functions')) % to use functions in different folder
 
 % % File for testing with sample ROS PointCloud2 variable
-load ~/catkin_ws/src/matlab/'Saved Variables'/ROS_PointCloud2.mat
+%load ~/catkin_ws/src/matlab/'Saved Variables'/ROS_PointCloud2.mat
 
 % Initiailze variables:
 threshold_value = 0.7;
 seq = 1;
 
 % create subscriber to depth cloud:
-%PointCloud_sub = rossubscriber('/camera/depth_registered/points');
+PointCloud_sub = rossubscriber('/camera/depth_registered/points');
 
 % create messages to pass the depth and rbg images
 depth = rosmessage('sensor_msgs/Image');
@@ -37,13 +37,13 @@ depth.Encoding = 'rgb16';
 color.Header.FrameId = 'lines_camera';
 depth.Header.FrameId = 'lines_camera';
 
-% while 1
+ while 1
 
     % Get data from ROS
-    %ptcloud_ros = receive(PointCloud_sub,10); % recieve the ROS cloud
-    %ptcloud_ros.PreserveStructureOnRead = true;
+    ptcloud_ros = receive(PointCloud_sub,10); % recieve the ROS cloud
+    ptcloud_ros.PreserveStructureOnRead = true;
     
-while 1
+%while 1
     im_ros = readRGB(ptcloud_ros); % create an rgb image from PointCloud
     depth_ros = readXYZ(ptcloud_ros); % create depth image from PointCloud
 
