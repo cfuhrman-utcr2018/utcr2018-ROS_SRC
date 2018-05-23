830c4d8c329245e5f3ddab73e7ffe665d75c911a
Connor <cfuhrman@citadel.edu>
2018-05-23 08:17:48 -0400

    sync

--- matlab/Scripts/RealSense Lane Detect/realsense_line_detect.m
+++ matlab/Scripts/RealSense Lane Detect/realsense_line_detect.m
@@ -15,13 +15,14 @@ rosinit
 addpath(genpath('Functions')) % to use functions in different folder
 
 % % File for testing with sample ROS PointCloud2 variable
-% load ~/catkins_ws/src/matlab/'Saved Variables'/ROS_PointCloud2.mat
+load ~/catkin_ws/src/matlab/'Saved Variables'/ROS_PointCloud2.mat
 
 % Initiailze variables:
 threshold_value = 0.7;
+seq = 1;
 
 % create subscriber to depth cloud:
-PointCloud_sub = rossubscriber('/camera/depth_registered/points');
+%PointCloud_sub = rossubscriber('/camera/depth_registered/points');
 
 % create messages to pass the depth and rbg images
 depth = rosmessage('sensor_msgs/Image');
@@ -31,7 +32,7 @@ depth_pub = rospublisher('/processed_depth','sensor_msgs/Image');
 color_pub = rospublisher('/processed_color', 'sensor_msgs/Image'); 
 
 color.Encoding = 'rgb8';
-depth.Encoding = 'rgb8';
+depth.Encoding = 'rgb16';
 
 color.Header.FrameId = 'lines_camera';
 depth.Header.FrameId = 'lines_camera';
@@ -39,9 +40,10 @@ depth.Header.FrameId = 'lines_camera';
 % while 1
 
     % Get data from ROS
-    ptcloud_ros = receive(PointCloud_sub,10); % recieve the ROS cloud
-    ptcloud_ros.PreserveStructureOnRead = true;
-
+    %ptcloud_ros = receive(PointCloud_sub,10); % recieve the ROS cloud
+    %ptcloud_ros.PreserveStructureOnRead = true;
+    
+while 1
     im_ros = readRGB(ptcloud_ros); % create an rgb image from PointCloud
     depth_ros = readXYZ(ptcloud_ros); % create depth image from PointCloud
 
@@ -53,16 +55,21 @@ depth.Header.FrameId = 'lines_camera';
     % Convert the lines_image to RGB8 encoding
     lines_image = im2uint8(lines_image);
     % Convert xyz_lines into RGB8 encoding
-    xyz_lines = im2uint8(xyz_lines);
+    xyz_lines = im2uint16(xyz_lines);
     
-    time = rostime(now);
+    time = rostime('now');
     color.Header.Stamp = time;
     depth.Header.Stamp = time;
+    
+    color.Header.Seq = seq;
+    depth.Header.Seq = seq;
 
     writeImage(depth, xyz_lines);
     writeImage(color, lines_image);
-while 1
+
     send(depth_pub, depth);
     send(color_pub, color);
+    
+    seq = seq + 1;
 end
 % end
