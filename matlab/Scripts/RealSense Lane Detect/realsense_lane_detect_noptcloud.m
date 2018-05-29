414e79a977a2b97551cde323e7ecc9386fbf4785
Connor <cfuhrman@citadel.edu>
2018-05-29 12:57:45 -0400

    image processing done with only xyz image to PointCloud2 -- no rgbd cloud for improved speed

--- matlab/Scripts/RealSense Lane Detect/realsense_lane_detect_noptcloud.m
+++ matlab/Scripts/RealSense Lane Detect/realsense_lane_detect_noptcloud.m
@@ -20,29 +20,37 @@ seq = 1;
 
 color_sub = rossubscriber('/camera/color/image_raw'); 
 depth_sub = rossubscriber('/camera/depth/image_rect_raw');
+info_sub = rossubscriber('/camera/color/camera_info');
 
 % create messages to pass the depth image
 depth_msg = rosmessage('sensor_msgs/Image');
-depth_pub = rospublisher('/processed_depth','sensor_msgs/Image');
+depth_pub = rospublisher('/matlab/processed_depth','sensor_msgs/Image');
+info_msg = rosmessage('sensor_msgs/CameraInfo');
+info_pub = rospublisher('/matlab/camera_info', 'sensor_msgs/CameraInfo');
 
-depth_msg.Encoding = '16uc1';
-
+depth_msg.Encoding = '32FC1';
 depth_msg.Header.FrameId = 'lines_link';
 
  while 1
      color_ros = receive(color_sub);
      depth_ros = receive(depth_sub);
+     info_ros = receive(info_sub);
+     info_msg = info_ros;
      
      color = readImage(color_ros);
      depth = readImage(depth_ros);
      
     % Detect white lines and return an image showing only white lines:
     [lines_image, xyz_lines] = process_image(color, depth, threshold_value);
+    xyz_lines = im2single(xyz_lines); % Convert to the 32fc1 image encoding
  
     time = rostime('now');
     depth_msg.Header.Stamp = time;
     depth_msg.Header.Seq = seq;
+    info_msg.Header.Stamp = time;
+    info_msg.Header.Seq = seq;
     writeImage(depth_msg, xyz_lines);
-    send(depth_pub, depth_msg);    
+    send(depth_pub, depth_msg);
+    send(info_pub, info_msg);
     seq = seq + 1;
  end
